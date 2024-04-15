a08554aa4f0252d79ae4677b19313ab8afb258e8
NemesisGoat <82102672+NemesisGoat@users.noreply.github.com>
2024-04-15 23:55:00 +0400

    Проблемы с Графом блин(

--- src/modules/Graph/Graph.js
+++ src/modules/Graph/Graph.js
@@ -1,90 +1,102 @@
-function Graph(options) {
-    const { id, width = 300, height = 300, WIN, callbacks = {} } = options;
-    let canvas;
-    if (id) {
-        canvas = document.getElementById(id);
-    } else {
-        canvas = document.createElement('canvas');
-        document.querySelector('body').appendChild(canvas);
-    }
-
-    canvas.width = width;
-    canvas.height = height;
-    const context = canvas.getContext('2d');
-
-    canvas.addEventListener('wheel', callbacks.wheel);
-    canvas.addEventListener('mousedown', callbacks.mousedown);
-    canvas.addEventListener('mousemove', callbacks.mousemove);
-    canvas.addEventListener('mouseup', callbacks.mouseup);
-    canvas.addEventListener('mouseleave', callbacks.mouseleave);
-    canvas.addEventListener('mousemove', callbacks.getX);
-
-    //Отображение координаты x на canvas
-    const xs = (x) => ((x - WIN.LEFT) / WIN.WIDTH) * canvas.width;
-    const xsPolyhon = (x) => (x / WIN.WIDTH) * canvas.width;
-
-    //Отображение координаты y на canvas
-    const ys = (y) => (WIN.HEIGHT - (y - WIN.BOTTOM)) * canvas.width / WIN.HEIGHT;
-    const ysPolygon = (y) => (WIN.HEIGHT - y) * canvas.width / WIN.HEIGHT;
-
-    //xs наоборот
-    this.sx = x => x * WIN.WIDTH / canvas.width + WIN.LEFT;
-
-    //ys наоборот
-    this.sy = y => -y * WIN.HEIGHT / canvas.height - WIN.BOTTOM;
-
-    this.clear = () => {
-        context.clearRect(0, 0, canvas.width, canvas.height)
-    }
-
-    this.line = (x1, y1, x2, y2, color = 'black', width = 1) => {
-        context.beginPath();
-        context.strokeStyle = color;
-        context.lineWidth = width;
-        context.moveTo(xs(x1), ys(y1));
-        context.lineTo(xs(x2), ys(y2));
-        context.stroke();
-        context.closePath();
-    }
-
-    this.point = (x, y, color = 'black', size = 2) => {
-        context.beginPath();
-        context.strokeStyle = color;
-        context.fillStyle = color;
-        context.arc(xs(x), ys(y), size, 0, 2 * Math.PI);
-        context.stroke();
-        context.fill();
-        context.closePath();
-    }
-
-    this.print = (x, y, text, color, size) => {
-        context.font = size / WIN.WIDTH + "px Verdana";
-        context.fillStyle = color;
-        context.fillText(text, xs(x), ys(y));
-        context.stroke;
-    }
-
-    this.dashedLine = (x1, y1, x2, y2, color, width) => {
-        context.beginPath();
-        context.setLineDash([20, 5]);
-        context.strokeStyle = color || 'black';
-        context.lineWidth = width || 4;
-        context.moveTo(xs(x1), ys(y1));
-        context.lineTo(xs(x2), ys(y2));
-        context.stroke();
-        context.closePath();
-        context.setLineDash([0, 0]);
-    }
-
-    this.polygon = function (points, color = '#f805') {
-        context.fillStyle = color;
-        context.beginPath();
-        context.moveTo(xs(points[0].x), ys(points[0].y));
-        for (let i = 1; i < points.length; i++){
-            context.lineTo(xs(points[i].x), ys(points[i].y));
+class Graph {
+    constructor({ id, width = 300, height = 300, WIN, callbacks = {} }) {
+        let canvas;
+        this.WIN = WIN;
+        if (id) {
+            canvas = document.getElementById(id);
+        } else {
+            canvas = document.createElement('canvas');
+            document.querySelector('body').appendChild(canvas);
         }
-        context.lineTo(xs(points[0].x), ys(points[0].y));
-        context.closePath();
-        context.fill();
+        canvas.width = width;
+        canvas.height = height;
+        this.canvas = canvas;
+        this.context = canvas.getContext('2d');
+        const { wheel, mousemove, mouseleave, mouseup, mousedown } = callbacks;
+        canvas.addEventListener('wheel', wheel);
+        canvas.addEventListener('mousemove', mousemove);
+        canvas.addEventListener('mouseleave', mouseleave);
+        canvas.addEventListener('mouseup', mouseup);
+        canvas.addEventListener('mousedown', mousedown);
+        this.PI2 = 2 * Math.PI;
+
+        this.canvasV = document.createElement('canvas');
+        this.canvasV.width = width;
+        this.canvasV.height = height;
+        this.contextV = this.canvasV.getContext('2d');
+    }
+
+    xs(x) {
+        return (x - this.WIN.LEFT) / this.WIN.WIDTH * this.canvasV.width;
+    }
+
+    ys(y) {
+        return (this.WIN.HEIGHT - (y - this.WIN.BOTTOM)) / this.WIN.HEIGHT * this.canvasV.height;
+    }
+
+    sx(x) {
+        return x * this.WIN.WIDTH / this.canvasV.width;
+    }
+
+    sy(y) {
+        return -y * this.WIN.HEIGHT / this.canvasV.height;
+    }
+
+
+    clear () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    }
+
+    line(x1, y1, x2, y2, color = 'purple', width = 2) {
+        this.contextV.beginPath();
+        this.contextV.strokeStyle = color;
+        this.contextV.lineWidth = width;
+        this.contextV.moveTo(this.xs(x1), this.ys(y1));
+        this.contextV.lineTo(this.xs(x2), this.ys(y2));
+        this.contextV.stroke();
+        this.contextV.closePath();
+    }
+
+    point (x, y, color = 'black', size = 2) {
+        this.contextV.beginPath();
+        this.contextV.strokeStyle = color;
+        this.contextV.fillStyle = color;
+        this.contextV.arc(this.xs(x), this.ys(y), size, 0, 2 * Math.PI);
+        this.contextV.stroke();
+        this.contextV.fill();
+        this.contextV.closePath();
+    }
+
+    print (x, y, text, color, size) {
+        this.contextV.font = size / this.WIN.WIDTH + "px Verdana";
+        this.contextV.fillStyle = color;
+        this.contextV.fillText(text, this.xs(x), this.ys(y));
+        this.contextV.stroke();
+    }
+
+    dashedLine (x1, y1, x2, y2, color, width) {
+        this.contextV.beginPath();
+        this.contextV.setLineDash([20, 5]);
+        this.contextV.strokeStyle = color || 'black';
+        this.contextV.lineWidth = width || 4;
+        this.contextV.moveTo(this.xs(x1), this.ys(y1));
+        this.contextV.lineTo(this.xs(x2), this.ys(y2));
+        this.contextV.stroke();
+        this.contextV.closePath();
+        this.contextV.setLineDash([0, 0]);
+    }
+
+    polygon(points, color = '#f805') {
+        this.contextV.fillStyle = color;
+        this.contextV.beginPath();
+        this.contextV.moveTo(this.xs(points[0].x), this.ys(points[0].y));
+        for (let i = 1; i < points.length; i++) {
+            this.contextV.lineTo(this.xs(points[i].x), this.ys(points[i].y));
+        }
+        this.contextV.lineTo(this.xs(points[0].x), this.ys(points[0].y));
+        this.contextV.closePath();
+        this.contextV.fill();
     }
 }
+
+export default Graph;
--- src/modules/Graph/UI.js
+++ src/modules/Graph/UI.js
@@ -1,57 +1,78 @@
-function UI({ addFunction, delFunction, colorFunction, getPerivativeOn }) {
-    let num = 1;
-    document.getElementById('addFunction').addEventListener('click', addClickHandler);
+import React from "react";
 
-    addClickHandler();
+class UI extends React.Component {
+    constructor(options) {
+        super(options);
+        const {
+            addFunction,
+            delFunction,
+            colorFunction,
+            getPerivativeOn
+        } = options.callbacks;
 
-    function addClickHandler() {
-        createFunctionInput();
+        this.num = 1;
+        this.addFunction = addFunction;
+        this.delFunction = delFunction;
+        this.setColor = colorFunction;
+        this.getPerivativeOn = getPerivativeOn;
+
+        this.addClickHandler();
     }
 
-    function keyupHandler() {
+    addEventListeners() {
+        document.getElementById('addFunction')
+            .addEventListener('click', this.addClickHandler.bind(this));
+    }
+
+
+    addClickHandler() {
+        this.createFunctionInput();
+    }
+
+    keyupHandler() {
         try {
             let f;
             eval(`f = function(x) {return ${this.value}}`);
-            addFunction(f, this.dataset.num - 0);
+            this.addFunction(f, this.dataset.num - 0);
         } catch (e) {
             console.log('Ошибка ввода');
         }
     }
 
-    function createFunctionInput() {
+    createFunctionInput() {
         const container = document.createElement('div');
         container.classList.add('funcContainer');
 
         const input = document.createElement('input');
-        input.setAttribute('placeholder', 'Функция №' + num);
-        input.dataset.num = num;
+        input.setAttribute('placeholder', 'Функция №' + this.num);
+        input.dataset.num = this.num;
         input.classList.add('funcInputs');
-        input.addEventListener('keyup', keyupHandler);
+        input.addEventListener('keyup', this.keyupHandler);
 
         const changeColor = document.createElement('input');
         changeColor.type = 'color';
         changeColor.classList.add('changeColor');
-        changeColor.dataset.num = num;
-        changeColor.addEventListener('input', getColor)
+        changeColor.dataset.num = this.num;
+        changeColor.addEventListener('input', this.getColor)
 
         const getPerivative = document.createElement('button');
         getPerivative.classList.add('optionsButton');
         getPerivative.innerHTML = ('Искать касательную');
         getPerivative.id = 'getPerivative';
-        getPerivative.dataset.num = num;
-        getPerivative.addEventListener('click', getPerivativeClickHandler);
+        getPerivative.dataset.num = this.num;
+        getPerivative.addEventListener('click', this.getPerivativeClickHandler);
 
-        const getIntegral = document.createElement('button');
-        getIntegral.classList.add('optionsButton');
-        getIntegral.innerHTML = ('Найти площадь');
-        getIntegral.id = 'getIntegral';
-        getIntegral.addEventListener('click', getIntegralClickHandler);
+        // const getIntegral = document.createElement('button');
+        // getIntegral.classList.add('optionsButton');
+        // getIntegral.innerHTML = ('Найти площадь');
+        // getIntegral.id = 'getIntegral';
+        // getIntegral.addEventListener('click', this.getIntegralClickHandler);
 
         const button = document.createElement('button');
         button.classList.add('deleteButton');
         button.innerHTML = 'X';
         button.addEventListener('click', () => {
-            delFunction(input.dataset.num - 0);
+            this.delFunction(input.dataset.num - 0);
             container.removeChild(input);
             container.removeChild(button);
             funcInputs.removeChild(container);
@@ -63,35 +84,37 @@ function UI({ addFunction, delFunction, colorFunction, getPerivativeOn }) {
         container.appendChild(input);
         container.appendChild(button);
         container.appendChild(getPerivative)
-        container.appendChild(getIntegral)
-        num++;
+        // container.appendChild(getIntegral)
+        this.num++;
     }
 
-    function getColor() {
-        colorFunction(this.value, this.dataset.num - 0);
+    getColor() {
+        this.colorFunction(this.value, this.dataset.num - 0);
     }
 
-    function getPerivativeClickHandler() {
-        getPerivativeOn(this.dataset.num - 0);
+    getPerivativeClickHandler() {
+        this.getPerivativeOn(this.dataset.num - 0);
     }
 
-    function getIntegralClickHandler() {
-        const parametersDialog = document.createElement('dialog');
-        parametersDialog.classList.add('parametersDialog');
-        document.querySelector('body').appendChild(parametersDialog);
-        parametersDialog.showModal();
+    // getIntegralClickHandler() {
+    //     const parametersDialog = document.createElement('dialog');
+    //     parametersDialog.classList.add('parametersDialog');
+    //     document.querySelector('body').appendChild(parametersDialog);
+    //     parametersDialog.showModal();
 
-        const button = document.createElement('button');
-        button.classList.add('deleteButton');
-        button.innerHTML = 'X';
-        button.addEventListener('click', () => {
-            parametersDialog.close()
-        });
+    //     const button = document.createElement('button');
+    //     button.classList.add('deleteButton');
+    //     button.innerHTML = 'X';
+    //     button.addEventListener('click', () => {
+    //         parametersDialog.close()
+    //     });
 
-        description = document.createElement('h1');
-        description.innerHTML = 'Нахождение площади криволинейной трапеции';
-        parametersDialog.appendChild(description);
-        parametersDialog.appendChild(button);
-    }
+    //     description = document.createElement('h1');
+    //     description.innerHTML = 'Нахождение площади криволинейной трапеции';
+    //     parametersDialog.appendChild(description);
+    //     parametersDialog.appendChild(button);
+    // }
 
 }
+
+export default UI;
